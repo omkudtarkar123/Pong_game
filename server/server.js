6b5fd2e45f4afdf2b7fc6841a13212b33e36d640
Om Kudtarkar <omkudtarkar@oms-macbook-pro.lan>
2022-01-27 21:48:25 -0800

    server fixes

--- server/server.js
+++ server/server.js
@@ -23,13 +23,15 @@ const io = require("socket.io")(server, {
   handlePreflightRequest: (req, res) => {
       const headers = {
           "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Origin": 'https://adoring-hodgkin-d40b8b.netlify.app/', //or the specific origin you want to give access to,
+          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
           "Access-Control-Allow-Credentials": true
       };
       res.writeHead(200, headers);
       res.end();
   }
 });
+
+io.set('origins', io.set('origins', 'http://yourdomain.com:80'));
 
 const { gameLoop, getUpdatedVelocity, initGame, } = require('./game')
 const { FRAME_RATE } = require('./constants')
